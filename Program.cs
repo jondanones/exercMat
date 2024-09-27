85f1b60870c66040cf91b88de453844c6bec60d7
Jow <john.sama64@gmail.com>
2024-09-27 20:33:14 -0300

    Exercício referente a matrizes

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int[,] mat = new int[n, n];
+
+for (int i = 0; i < n; i++) {
+    string[] vet = Console.ReadLine().Split(' ');
+    for (int j = 0; j < n; j++) {
+        mat[i, j] = int.Parse(vet[j]);
+    }
+}
+
+Console.WriteLine("Main diagonal: ");
+for (int i = 0; i < n; i++) {
+    Console.Write(mat[i, i] + " ");
+}
+Console.WriteLine();
+
+int count = 0;
+for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+        if (mat[i, j] < 0) {
+            count++;
+        }
+    }
+}
+Console.WriteLine("Negative numbers: " + count);
